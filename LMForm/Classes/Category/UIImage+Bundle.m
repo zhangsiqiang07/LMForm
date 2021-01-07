ff42702b574642f029fd173095860a518844f0c2
张思强 <1328254616@qq.com>
2021-01-08 05:28:48 +0800

    Update UIImage+Bundle.m

--- LMForm/Classes/Category/UIImage+Bundle.m
+++ LMForm/Classes/Category/UIImage+Bundle.m
@@ -18,9 +18,36 @@
 
 + (NSBundle *)bundleWithResourceName:(NSString *)resourceName
 {
-    NSURL *url = [[NSBundle mainBundle] URLForResource:resourceName withExtension:@"bundle"];
-    NSBundle *resouceBundle = [NSBundle bundleWithURL:url];
+  
+   return [self bundleWithBundleName:nil podName:resourceName];
+}
+
++ (NSBundle *)bundleWithBundleName:(NSString *)bundleName podName:(NSString *)podName{
+    if (bundleName == nil && podName == nil) {
+        @throw @"bundleName和podName不能同时为空";
+    }else if (bundleName == nil ) {
+        bundleName = podName;
+    }else if (podName == nil) {
+        podName = bundleName;
+    }
     
-    return resouceBundle;
+    
+    if ([bundleName containsString:@".bundle"]) {
+        bundleName = [bundleName componentsSeparatedByString:@".bundle"].firstObject;
+    }
+    //没使用framwork的情况下
+    NSURL *associateBundleURL = [[NSBundle mainBundle] URLForResource:bundleName withExtension:@"bundle"];
+    //使用framework形式
+    if (!associateBundleURL) {
+        associateBundleURL = [[NSBundle mainBundle] URLForResource:@"Frameworks" withExtension:nil];
+        associateBundleURL = [associateBundleURL URLByAppendingPathComponent:podName];
+        associateBundleURL = [associateBundleURL URLByAppendingPathExtension:@"framework"];
+        NSBundle *associateBunle = [NSBundle bundleWithURL:associateBundleURL];
+        associateBundleURL = [associateBunle URLForResource:bundleName withExtension:@"bundle"];
+    }
+    
+    NSAssert(associateBundleURL, @"取不到关联bundle");
+    //生产环境直接返回空
+    return associateBundleURL?[NSBundle bundleWithURL:associateBundleURL]:nil;
 }
 @end
